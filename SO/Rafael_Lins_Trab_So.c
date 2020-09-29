745accd80b2a6d652ff99f13b93a7b942e99e6cc
Rafael Lins Pinheiro <65605919+RafaelP9113@users.noreply.github.com>
2020-09-29 14:34:32 +0000

    Parte 3 concluida (Necessita de ajustes)

--- SO/Rafael_Lins_Trab_So.c
+++ SO/Rafael_Lins_Trab_So.c
@@ -7,6 +7,11 @@
 #include <stdlib.h>
 #include <semaphore.h>
 #include <time.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <errno.h>
+
 
 /*
     Linha para execucao no prompt
@@ -14,21 +19,27 @@
 
 */
 
+
 sem_t S;
 
-void avalia_vet(int * vet){
-    int *num = vet;    
+void avalia_vet(int * vet_mod, int * vet_comp){    
     int cont = 0;
     for (int i = 0; i < 100000; i++){
-        if((vet[i] != 0) && (vet[i] % 2) == 0){
+        if((vet_mod[i] != 0) && (vet_mod[i] % 2) == 0){
             cont += 1;
         }
-        if((vet[i] != 0) && (vet[i] % 5) == 0){
+        if((vet_mod[i] != 0) && (vet_mod[i] % 5) == 0){
             cont += 1;
+        }
+        if(((vet_comp[i] != 0) && (vet_comp[i] % 2) != 0) && ((vet_comp[i] != 0) && (vet_comp[i] % 5) != 0)){
+            if(vet_mod[i] == 0){
+                cont += 1;            
+            }        
         }
     }
     if (cont >= 1){
         printf("Deu ruim ai...\n");
+        printf("%d\n", cont);
     }
     else{
         printf("Tudo certo meu bom !\n");
@@ -51,8 +62,8 @@ void *removepar(void *arg){
     for (int i = 0; i < 100000; i++){
         int number = i - 99999;
         number = number * (-1);
-        if((num[i] % 2) == 0){
-            num[i] = 0; 
+        if((num[number] % 2) == 0){
+            num[number] = 0; 
         }
     }
 
@@ -63,8 +74,8 @@ void *remove5(void *arg){
     for (int i =0; i < 100000; i++){
         int number = i - 99999;
         number = number * (-1);
-        if((num[i] % 5) == 0){
-            num[i] = 0; 
+        if((num[number] % 5) == 0){
+            num[number] = 0; 
         }
     }
 }
@@ -112,70 +123,73 @@ void part1(){
 
 
 void part2_thread(){
-    int vetor[100000];
+    int vetor_modificado[100000], vetor_completo[100000];
     for (int i = 0; i < 100000; i++){
         int num = rand()%100;
         while (num == 0){
             num = rand()%100;
         }
-        vetor[i] = num;
+        vetor_modificado[i] = num;
+        vetor_completo[i] = num;
     }
 /*      for (int i = 0; i < 100000; i++){
-        printf("%d ", vetor[i]);
+        printf("%d ", vetor_modificado[i]);
       }  
 */
     pthread_t thread_par, thread_5;
-    pthread_create(&(thread_par), NULL, removepar, vetor);
-    pthread_create(&(thread_5), NULL, remove5, vetor);
+    pthread_create(&(thread_par), NULL, removepar, vetor_modificado);
+    pthread_create(&(thread_5), NULL, remove5, vetor_modificado);
     pthread_join((thread_par),NULL);
     pthread_join((thread_5),NULL);
 
 /*      for (int i = 0; i < 100000; i++){
-        printf("%d ", vetor[i]);
+        printf("%d ", vetor_modificado[i]);
       }  
 */
-    avalia_vet(vetor);    
+    avalia_vet(vetor_modificado, vetor_completo);    
 }
 
 void part2_semaphore(){
-    int vetor[100000];
+    int vetor_modificado[100000], vetor_completo[100000];
     for (int i = 0; i < 100000; i++){
         int num = rand()%100;
         while (num == 0){
             num = rand()%100;
         }
-        vetor[i] = num;
+        vetor_modificado[i] = num;
+        vetor_completo[i] = num;
     }
 /*      for (int i = 0; i < 100000; i++){
-        printf("%d ", vetor[i]);
+        printf("%d ", vetor_modificado[i]);
       }  
 */
     pthread_t thread_par_semaphore,thread_5_semaphore ;
-    pthread_create(&(thread_par_semaphore), NULL, removepar_semaphore, vetor);
-    pthread_create(&(thread_5_semaphore), NULL, remove5_semaphore, vetor);
+    pthread_create(&(thread_par_semaphore), NULL, removepar_semaphore, vetor_modificado);
+    pthread_create(&(thread_5_semaphore), NULL, remove5_semaphore, vetor_modificado);
     pthread_join((thread_par_semaphore),NULL);
     pthread_join((thread_5_semaphore),NULL);
     printf("\n");
 /*      for (int i = 0; i < 100000; i++){
-        printf("%d ", vetor[i]);
+        printf("%d ", vetor_modificado[i]);
       }  
 */
-    avalia_vet(vetor);
+    avalia_vet(vetor_modificado, vetor_completo);
 }
 
 void part2(){
-    int vetor[100000];
+    int vetor_modificado[100000], vetor_completo[100000];
     for (int i = 0; i < 100000; i++){
         int num = rand()%100;
         while (num == 0){
             num = rand()%100;
         }
-        vetor[i] = num;
+        vetor_modificado[i] = num;
+        vetor_completo[i] = num;
     }
-    removepar(vetor);
-    remove5(vetor);
+    removepar(vetor_modificado);
+    remove5(vetor_modificado);
   
-    avalia_vet(vetor);
+    avalia_vet(vetor_modificado, vetor_completo);
 }
 
 void EX01(){
@@ -201,7 +215,47 @@ void EX02(){
     printf("\n");
 }
 
+void EX03(){
+    clock_t t1;
+    t1 = clock();
+    int vetor_modificado[100000], vetor_completo[100000];
+    for (int i = 0; i < 100000; i++){
+        int num = rand()%100;
+        while (num == 0){
+            num = rand()%100;
+        }
+        vetor_modificado[i] = num;
+        vetor_completo[i] = num;
+    }
+   
+    pid_t child_pid;
+    int child_status;
+    child_pid = fork();
+  
+    switch (child_pid){
+        case -1:
+            perror("fork");
+            exit(1);
+        case 0:
+            remove5(vetor_modificado);
+            
+            exit(0);
+        default:
+            //printf("eae antes\n"); faz primeiro de tudo
+            remove5(vetor_modificado);
+            wait(&child_status); 
+            removepar(vetor_modificado);
+            avalia_vet(vetor_modificado, vetor_completo);
+    
+            //printf("eae depois\n"); faz quando acabar processo filho  
+            
+    }
+    t1 = clock() - t1;
+    printf("com processos %lf ms\n", ((double)t1)/((CLOCKS_PER_SEC/1000)));
+}
+
 int main(void){
     EX01();
     EX02();
+    EX03();
 }
